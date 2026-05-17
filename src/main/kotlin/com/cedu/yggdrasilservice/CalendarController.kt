17eee9ffc234e37a5dc573f5c9247a604cd05ddf
CanisExDaemonumUrbe <d.gelimyanov@yandex.ru>
2026-05-18 02:50:45 +0500

    ver 0.1.1 - fix общего префикса

--- src/main/kotlin/com/cedu/yggdrasilservice/CalendarController.kt
+++ src/main/kotlin/com/cedu/yggdrasilservice/CalendarController.kt
@@ -14,11 +14,13 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
-@RestController("/api/v1/yggdrasil")
+@RestController
+@RequestMapping("/api/v1/yggdrasil")
 class CalendarController(
     private val calendarService: CalendarService,
     private val authService: AuthService
